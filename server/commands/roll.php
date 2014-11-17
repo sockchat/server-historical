42bfc71db686bf515e54d533eadabd175ade5c85
kamil0 <kamil0@users.noreply.github.com>
2014-11-16 20:13:15 -0500

    Update roll.php

--- server/commands/roll.php
+++ server/commands/roll.php
@@ -6,7 +6,9 @@ namespace sockchat\cmds;
 use sockchat\cmds\GenericCommand;
 class roll implements GenericCommand {
     public static function doCommand($chat, $user, $arr) {
+      if(isset($chat["ROLL_ENABLED"]) && $chat["ROLL_ENABLED"] == true) {
       $chat->BroadcastMessage($chat->chatbot, "<i><strong>" . $user . "</strong> rolled the dice and got " . mt_rand(1,10) . ".</i>");
-      // will add 1d1 etc later
+      // will add 1d1 etc 
+      }
     }
 }
