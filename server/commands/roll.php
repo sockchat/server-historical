287b52148064487ddd0315e36f9463582c186aa7
kamil0 <kamil0@users.noreply.github.com>
2014-11-16 20:08:02 -0500

    Create roll.php

---
+++ server/commands/roll.php
@@ -0,0 +1,12 @@
+<?php
+
+// Why not.
+
+namespace sockchat\cmds;
+use sockchat\cmds\GenericCommand;
+class roll implements GenericCommand {
+    public static function doCommand($chat, $user, $arr) {
+      $chat->BroadcastMessage($chat->chatbot, "<i><strong>" . $user . "</strong> rolled the dice and got " . mt_rand(1,10) . ".</i>");
+      // will add 1d1 etc later
+    }
+}
