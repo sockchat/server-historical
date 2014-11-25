de89be4179aadb272e233b7a2f505d6420547640
Kamil Rakowski <kamil0@users.noreply.github.com>
2014-11-24 19:40:03 -0500

    Now it should work

--- server/commands/roll.php
+++ server/commands/roll.php
@@ -4,7 +4,7 @@ use sockchat\cmds\GenericCommand;
 
 class roll implements GenericCommand {
     public static function doCommand($chat, $user, $arr) {
-        $chat->BroadcastMessage($chat->chatbot, "<i><strong>" . $user . "</strong> rolled the dice and got " . mt_rand(1,10) . ".</i>");
+        $chat->BroadcastMessage($chat->chatbot, "<i><strong>" . $user->username . "</strong> rolled the dice and got " . mt_rand(1,10) . ".</i>");
         // will add 1d1 etc
     }
 }
