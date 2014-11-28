5ea575f31f794da3710e7f1298ed4cc68f94c602
Kamil Rakowski <kamil0@users.noreply.github.com>
2014-11-28 17:10:06 -0500

    Update roll.php

--- server/commands/roll.php
+++ server/commands/roll.php
@@ -1,11 +1,3 @@
 <?php
-namespace sockchat\cmds;
-use sockchat\cmds\GenericCommand;
-
-class roll implements GenericCommand {
-    public static function doCommand($chat, $user, $arr) {
-        $chat->BroadcastMessage($chat->chatbot, "<i><strong>" . $user->username . "</strong> rolled the dice and got " . mt_rand(1,10) . ".</i>");
-        // CAMEL FIX YOUR FCKING SHIT
-        // will add 1d1 etc
-    }
-}
+// Nope
+?>
